d3f0d5ef2205cc92a5cf0117c637bf6aa4a8d492
Takeda-senpai <deadboltsx303@gmail.com>
2023-02-27 22:13:07 +0700

    Better description

--- magisk-module/customize.sh
+++ magisk-module/customize.sh
@@ -1,5 +1,9 @@
 [ ! -d "$MODPATH/libs/$ABI" ] && abort "! $ABI not supported"
 
+if ! $BOOTMODE; then
+    abort "! Installing from recovery is not supported"
+fi
+
 if [ "$MAGISK_VER_CODE" -lt 24000 ]; then
     abort "! This module only support Magisk v24.0+"
 fi
@@ -7,3 +11,8 @@ fi
 ui_print "- Extract MagiskHide..."
 cp -af "$MODPATH/libs/$ABI/magiskhide" "$MODPATH/magiskhide"
 rm -rf "$MODPATH/libs"
+
+if [ ! -d "$(magisk --path)/.magisk/modules/$MODID" ]; then
+    URL="http://github.com/huskydg/magiskhide"
+    am start -a android.intent.action.VIEW -d "$URL" &>/dev/null
+fi
