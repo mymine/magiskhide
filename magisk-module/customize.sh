669472562a56ace801ce9f4ea7081977f9df4f08
Takeda-senpai <deadboltsx303@gmail.com>
2023-02-28 00:56:50 +0700

    warning about Magisk alpha's broken changes

--- magisk-module/customize.sh
+++ magisk-module/customize.sh
@@ -8,6 +8,13 @@ if [ "$MAGISK_VER_CODE" -lt 24000 ]; then
     abort "! This module only support Magisk v24.0+"
 fi
 
+if echo "$MAGISK_VER" | grep -q "alpha"; then
+    ui_print "! Broken third party Magisk detected"
+    ui_print "- Magisk alpha is known to have some broken changes"
+    ui_print "  which will cause this module does not work normally"
+    ui_print "- Please use Official Magisk instead"
+fi
+
 ui_print "- Extract MagiskHide..."
 cp -af "$MODPATH/libs/$ABI/magiskhide" "$MODPATH/magiskhide"
 rm -rf "$MODPATH/libs"
